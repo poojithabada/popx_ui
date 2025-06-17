8ff8342cc7404fb45f24fa4d4f7014d80a525311
Bada Poojitha <poojithabada68@gmail.com>
2025-06-17 17:29:09 +0530

    chnaged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome/Welcome.js";
 import Login from "./pages/Login/Login.js";
 import Register from "./pages/Register/Register.js";
@@ -7,16 +7,18 @@ import Account from "./pages/Account/Account.js";
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="mobile-frame">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
+    <BrowserRouter basename="/popx_ui">
+      <div className="app-container">
+        <div className="mobile-frame">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
